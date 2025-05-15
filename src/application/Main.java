7783d2b12e3df6b00f9804c5aea02a7980041c5d
Wessel <wessel@autumnskies-online.co.za>
2025-05-15 23:33:05 +0200

    Test IntelliJ push functionality

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,20 +1,37 @@
-// JavaFX Calculator Application
-// Skeleton Starter Code
-
 package application;
 
 import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
 
     public static void main(String[] args) {
-        launch(args);
+        Application.launch(args);
     }
 
     @Override
-    public void start(Stage arg0) throws Exception {
-        // TODO Auto-generated method stub
+    public void start(Stage stage) throws Exception {
+
+        Group root = new Group();
+        Scene scene = new Scene(root, Color.GREY);
+
+        Image icon = new Image(getClass().getResourceAsStream("/aso_logo.png"));
+        stage.getIcons().add(icon);
+        stage.setTitle("ASO Calculator");
+
+        stage.setWidth(340);
+        stage.setHeight(500);
+        stage.setResizable(false);
+
+
+
+
+        stage.setScene(scene);
+        stage.show();
     }
 }
